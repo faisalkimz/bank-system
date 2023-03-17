8f9b2666a5d073b76ad8853fe3701984c3355e73
faisalkimz <mbabalifaisal2@gmail.com>
2023-03-17 15:42:46 +0300

    my trial system

---
+++ Online-BankingPHP/db_connect.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "bnksys";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+	header('location:server_down.php');
+} else {
+	// echo "Connection Established";
+}
+
+?>
